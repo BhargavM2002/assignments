35fc5e5d1cca4f8239a830ffd2d386ee71273ac5
Bhargav <152622731+BhargavM2002@users.noreply.github.com>
2025-02-12 19:45:20 +0530

    Update anagram.js

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -5,7 +5,22 @@
 */
 
 function isAnagram(str1, str2) {
-
+  const LCStr1=str1.toLowercase();
+  const a1=LCStr1.split("");
+  a1.sort()
+  const ss1=a1.join()
+  const LCStr2=str2.toLowercase();
+  const a2=LCStr2.split();
+  a2.sort();
+  const ss2=a2.join();
+  if(ss1 == ss2)
+  {
+    return true;
+  }
+  else
+  {
+  return false;
+  }
 }
 
 module.exports = isAnagram;
